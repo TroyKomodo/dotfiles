438cdf53b7227893fe022a83deea8f62feb3cf87
Troy Benson <troybensonsa@gmail.com>
2026-02-28 18:10:09 +0200

    fixes

--- system/hetzner-vpn.nix
+++ system/hetzner-vpn.nix
@@ -1,4 +1,17 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    "${modulesPath}/profiles/qemu-guest.nix"
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.useDHCP = true;
+
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
@@ -8,9 +21,6 @@
     fsType = "ext4";
   };
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4"];
-
+  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod"];
   system.stateVersion = "25.11";
 }
