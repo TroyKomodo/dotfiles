32b419ea27ef1f8ac9d89c71c2ac0a3910b1d2ec
Troy Benson <me@troymoder.dev>
2026-04-12 21:28:16 +0200

    try plymouth

--- system/framework.nix
+++ system/framework.nix
@@ -1,46 +1,4 @@
-{pkgs, ...}: let
-  loginPlymouthTheme = pkgs.runCommand "plymouth-login-theme" {
-    nativeBuildInputs = [ pkgs.imagemagick ];
-  } ''
-    themeDir=$out/share/plymouth/themes/login
-    mkdir -p $themeDir
-
-    cp ${../static/login.plymouth} $themeDir/login.plymouth
-    cp ${../static/login.script}   $themeDir/login.script
-
-    # Clear wallpaper (idle mode with snow)
-    magick ${../static/wallpaper.jpg} \
-      -resize 2560x1600^ -gravity center -extent 2560x1600 \
-      $themeDir/wallpaper.png
-
-    # Blurred + darkened wallpaper (password mode)
-    magick ${../static/wallpaper.jpg} \
-      -resize 2560x1600^ -gravity center -extent 2560x1600 \
-      -blur 0x10 \
-      $themeDir/wallpaper-blur.png
-
-    # Circular avatar
-    magick ${../static/pfp.jpg} \
-      \( +clone -alpha extract \
-        -draw "fill black polygon 0,0 0,%[fx:h/2] %[fx:w/2],0 fill white circle %[fx:w/2],%[fx:h/2] %[fx:w/2],0" \
-        \( +clone -flip \) -compose Multiply -composite \
-        \( +clone -flop \) -compose Multiply -composite \
-      \) -alpha off -compose CopyOpacity -composite \
-      $themeDir/avatar.png
-
-    # Password bullet
-    magick -size 24x24 xc:none -fill white -draw "circle 12,12 12,2" \
-      $themeDir/bullet.png
-
-    # Snowflake (soft white dot)
-    magick -size 16x16 xc:none -fill white -draw "circle 8,8 8,3" \
-      -blur 0x1.5 \
-      $themeDir/snow.png
-
-    substituteInPlace $themeDir/login.plymouth \
-      --replace /etc/plymouth-login $themeDir
-  '';
-in {
+{...}: {
   modules = {
     audio.enable = true;
     bluetooth.enable = true;
@@ -84,8 +42,7 @@ in {
 
   boot.plymouth = {
     enable = true;
-    theme = "login";
-    themePackages = [ loginPlymouthTheme ];
+    theme = "breeze";
   };
 
   boot.kernelParams = [ "quiet" "splash" "loglevel=3" "rd.udev.log_level=3" ];
